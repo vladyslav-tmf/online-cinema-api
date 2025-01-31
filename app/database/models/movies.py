50e0b5da71bd047f086600caf6d02f4374d597d7
Olha Stadnik <olgastadnik2507@gmail.comcd>
2025-01-31 16:14:23 +0200

    "create star and director model"

--- app/database/models/movies.py
+++ app/database/models/movies.py
@@ -12,3 +12,18 @@ class GenreModel(Base):
 
     movies = relationship("MovieModel", secondary="movie_genres", back_populates="genres")
 
+class StarModel(Base):
+    __tablename__ = "stars"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+
+    movies = relationship("MovieModel", secondary="movie_stars", back_populates="stars")
+
+class DirectorModel(Base):
+    __tablename__ = "directors"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+
+    movies = relationship("MovieModel", secondary="movie_directors", back_populates="directors")

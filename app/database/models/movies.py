715b33e37f505d9496dea970b90069006a45bc2f
Olha Stadnik <olgastadnik2507@gmail.comcd>
2025-01-31 16:21:13 +0200

    "create certification model"

--- app/database/models/movies.py
+++ app/database/models/movies.py
@@ -7,23 +7,43 @@ from app.database.models.base import Base
 class GenreModel(Base):
     __tablename__ = "genres"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True,
+                                    autoincrement=True)
     name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
 
-    movies = relationship("MovieModel", secondary="movie_genres", back_populates="genres")
+    movies = relationship("MovieModel", secondary="movie_genres",
+                          back_populates="genres")
+
 
 class StarModel(Base):
     __tablename__ = "stars"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True,
+                                    autoincrement=True)
     name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
 
-    movies = relationship("MovieModel", secondary="movie_stars", back_populates="stars")
+    movies = relationship("MovieModel", secondary="movie_stars",
+                          back_populates="stars")
+
 
 class DirectorModel(Base):
     __tablename__ = "directors"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True,
+                                    autoincrement=True)
     name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
 
-    movies = relationship("MovieModel", secondary="movie_directors", back_populates="directors")
+    movies = relationship("MovieModel", secondary="movie_directors",
+                          back_populates="directors")
+
+
+class CertificationModel(Base):
+    __tablename__ = "certifications"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True,
+                                    autoincrement=True)
+    name: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+
+    movies = relationship("MovieModel",
+                          back_populates="certifications")
+

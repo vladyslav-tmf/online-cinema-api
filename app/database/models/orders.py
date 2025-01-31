27d5e877317097cbf3630cb95a1629143a4aa9a2
user <nikitaskrypets@gmail.com>
2025-01-31 15:08:18 +0200

    create OrderModel and OrderItemModel

---
+++ app/database/models/orders.py
@@ -0,0 +1,43 @@
+import datetime
+from decimal import Decimal
+from enum import Enum
+
+from sqlalchemy import Numeric, ForeignKey, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Enum as SQLAlchemyEnum
+
+from database.models.base import Base
+
+
+class OrderStatusEnum(str, Enum):
+    PENDING = "pending"
+    PAID = "paid"
+    CANCELED = "canceled"
+
+
+class OrderModel(Base):
+    __tablename__ = "orders"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
+    status: Mapped[OrderStatusEnum] = mapped_column(
+        SQLAlchemyEnum(OrderStatusEnum), nullable=False
+    )
+    total_amount: Mapped[Decimal] = mapped_column(
+        Numeric(10, 2), nullable=True
+    )
+    order_item: Mapped[list["OrderItemModel"]] = relationship(
+        "OrderItemModel", back_populates="order"
+    )
+
+
+class OrderItemModel(Base):
+    __tablename__ = "order_items"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"), nullable=False)
+    movie_id: Mapped[int] = mapped_column(ForeignKey("movies.id"), nullable=False)
+    price_at_order: Mapped[Decimal] = mapped_column(Numeric(10, 2))

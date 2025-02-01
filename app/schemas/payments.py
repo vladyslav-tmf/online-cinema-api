553ff9bcfabb04699a428e1e9185347812f09bbe
user <nikitaskrypets@gmail.com>
2025-02-01 13:10:40 +0200

    created schema for payment and payment item

--- app/schemas/payments.py
+++ app/schemas/payments.py
@@ -4,13 +4,43 @@ from decimal import Decimal
 from pydantic import BaseModel
 
 from database.models.payments import PaymentStatusEnum
+from schemas.orders import OrderSchema, OrderItemSchema
+from schemas.users import UserSchema
 
 
-class PaymentSchema(BaseModel):
+class PaymentListSchema(BaseModel):
     created_at: datetime.datetime
     amount: Decimal
     status: PaymentStatusEnum
 
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class PaymentSchema(BaseModel):
+    id: int
+    user_id: int
+    order_id: int
+    created_at: datetime.datetime
+    status: PaymentStatusEnum
+    amount: Decimal
+    user: UserSchema
+    order: OrderSchema
+    items: list["PaymentItemSchema"]
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class PaymentItemSchema(BaseModel):
+    id: int
+    payment_id: int
+    order_item_id: int
+    price_at_payment: Decimal
+    payment: PaymentSchema
+    order_item: OrderItemSchema
     model_config = {
         "from_attributes": True
     }

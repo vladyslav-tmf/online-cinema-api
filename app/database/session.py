cfab34970f878f942551fb29bd88cc13caad0f5b
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2025-02-03 16:15:12 +0200

    Fix database configuration

--- app/database/session.py
+++ app/database/session.py
@@ -1,3 +1,4 @@
+import os
 from typing import Generator
 
 from sqlalchemy import create_engine
@@ -7,13 +8,21 @@ from app.config.settings import get_settings
 
 settings = get_settings()
 
-DATABASE_URL = (
-    f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@"
-    f"{settings.POSTGRES_HOST}:{settings.POSTGRES_DB_PORT}/{settings.POSTGRES_DB}"
-)
+IN_DOCKER = os.environ.get("DOCKER_ENV", False)
+
+if IN_DOCKER:
+    DATABASE_URL = (
+        f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@"
+        f"{settings.POSTGRES_HOST}:{settings.POSTGRES_DB_PORT}/{settings.POSTGRES_DB}"
+    )
+else:
+    DATABASE_URL = (
+        f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@"
+        f"localhost:{settings.POSTGRES_DB_PORT}/{settings.POSTGRES_DB}"
+    )
+
 engine = create_engine(DATABASE_URL)
-connection = engine.connect()
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=connection)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def get_db() -> Generator[Session, None, None]:

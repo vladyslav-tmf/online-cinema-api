bf839fa7bf7d36548be63f5b64fe8b45cd369c2e
user <nikitaskrypets@gmail.com>
2025-02-03 14:31:48 +0200

    implemented tests base structure

---
+++ app/tests/conftest.py
@@ -0,0 +1,81 @@
+import pytest
+from sqlalchemy import insert
+from starlette.testclient import TestClient
+
+from config.dependencies import get_s3_storage_client, get_accounts_email_notificator
+from app.config.settings import get_settings
+from app.database.models.accounts import UserGroupModel, UserGroupEnum
+from app.database.session import get_db
+from app.main import app
+from app.security.token_manager import JWTAuthManager
+from app.storages.s3 import S3StorageClient
+from app.tests.doubles.stubs.emails import StubEmailSender
+
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "e2e: End-to-end tests"
+    )
+    config.addinivalue_line(
+        "markers", "order: Specify the order of test execution"
+    )
+    config.addinivalue_line(
+        "markers", "unit: Unit tests"
+    )
+
+
+@pytest.fixture(scope="session")
+def settings():
+    return get_settings()
+
+
+@pytest.fixture(scope="function")
+def email_sender_stub():
+    return StubEmailSender()
+
+
+@pytest.fixture(scope="session")
+def s3_client(settings):
+    return S3StorageClient(
+        endpoint_url=settings.S3_STORAGE_ENDPOINT,
+        access_key=settings.S3_STORAGE_ACCESS_KEY,
+        secret_key=settings.S3_STORAGE_SECRET_KEY,
+        bucket_name=settings.S3_BUCKET_NAME
+    )
+
+
+@pytest.fixture(scope="function")
+def client():
+    with TestClient(app) as test_client:
+        yield test_client
+
+    app.dependency_overrides.clear()
+
+
+@pytest.fixture(scope="session")
+def e2e_client():
+    with TestClient(app) as test_client:
+        yield test_client
+
+
+@pytest.fixture(scope="function")
+def db_session():
+    with get_db() as session:
+        yield session
+
+
+@pytest.fixture(scope="function")
+def jwt_manager(settings):
+    return JWTAuthManager(
+        secret_key_access=settings.SECRET_KEY_ACCESS,
+        secret_key_refresh=settings.SECRET_KEY_REFRESH,
+        algorithm=settings.JWT_SIGNING_ALGORITHM
+    )
+
+
+@pytest.fixture(scope="function")
+def seed_user_groups(db_session):
+    groups = [{"name": group.value} for group in UserGroupEnum]
+    db_session.execute(insert(UserGroupModel).values(groups))
+    db_session.commit()
+    yield db_session

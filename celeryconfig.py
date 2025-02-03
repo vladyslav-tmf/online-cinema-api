efaaf19bed8db05d98807eea5dd157e64ba2c6dc
Max Savenya <savenya2013@gmail.com>
2025-02-03 12:54:26 +0200

    fix: Update celery config

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,8 +1,13 @@
 from celery.schedules import crontab
 
+broker_url = "redis://localhost:6379/0"
+result_backend = "redis://localhost:6379/0"
+
 CELERY_BEAT_SCHEDULE = {
     "delete_expired_activation_tokens_every_hour": {
         "task": "tasks.delete_expired_activation_tokens",
         "schedule": crontab(minute=0, hour="*/1"),
     },
 }
+
+timezone = "UTC"

7140db2f613a63f54a06bb8bfb3bf05c4e570ce4
Max Savenya <savenya2013@gmail.com>
2025-02-04 15:06:10 +0200

    fix: Update celery config to run every 26 hours

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,4 +1,4 @@
-from celery.schedules import crontab
+from celery.schedules import timedelta
 
 broker_url = "redis://localhost:6379/0"
 result_backend = "redis://localhost:6379/0"
@@ -6,7 +6,7 @@ result_backend = "redis://localhost:6379/0"
 CELERY_BEAT_SCHEDULE = {
     "delete_expired_activation_tokens_every_hour": {
         "task": "tasks.delete_expired_activation_tokens",
-        "schedule": crontab(minute=0, hour="*/1"),
+        "schedule": timedelta(hours=26),
     },
 }
 
